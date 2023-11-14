b42217054c9b29bb4fb81a99bec938c5b7400b29
Mark Dudas <110264939+MarkDudas@users.noreply.github.com>
2023-11-14 13:21:50 +0800

    change the format salary

--- src/utilities/Utilities.tsx
+++ src/utilities/Utilities.tsx
@@ -17,11 +17,7 @@ export const years = Array.from(
   { length: new Date().getFullYear() - 1950 + 1 },
   (_, index) => 1950 + index
 );
-
 export const formatSalary = (salary: number) => {
-  if (salary % 1000 === 0) {
-    return `₱ ${salary.toLocaleString()}`;
-  } else {
-    return `₱ ${salary}`;
-  }
+  return `₱ ${salary.toLocaleString()}`;
 };
+
